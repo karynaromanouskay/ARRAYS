6039783dcaa5332eb87484ef283d5d30bb1e02f1
karina <karynaromanouskaya@gmail.com>
2024-05-15 13:34:20 +0300

    src/

---
+++ src/main/ru/netology/stats/StatsService.java.js
@@ -0,0 +1,37 @@
+package ru.netology.stats;
+
+public class StatsService {
+    public long sunSales(long[] sales) {
+    long sun = 0;
+    for (long sale : sales) {
+    sun += sale;
+}
+return sun;
+}
+
+public long averageSales(long[] sales) {
+    return sunSales(sales) / sales.length;
+}
+
+public int minSales(long[] sales) {
+    int minMonth = 0; // номер месяца с минимальными продажами среди просмотренных ранее
+
+    for (int i = 0; i < sales.length; i++) {
+        if (sales[i] <= sales[minMonth]) { // значит, в рассматриваемом i-м месяце продаж меньше
+            minMonth = i; // запомним его как минимальный
+        }
+    }
+
+    return minMonth + 1; // месяца нумеруются с 1, а индексы массива с 0, нужно сдвинуть ответ на 1
+}
+public int maxSales(long[] sales) {
+    int count = 0;
+    long avg = averageSales(sales);
+    for (long sale : sales) {
+        if (sale < avg) {
+            count++;
+        }
+    }
+    return count;
+}
+}
